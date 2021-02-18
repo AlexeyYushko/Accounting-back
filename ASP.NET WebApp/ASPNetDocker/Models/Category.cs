fcdafe31d64fa499ca8226d887b6b552cd5d7ba2
Alexey Yushko <aleksei.jusko@dfi.ai>
2021-02-18 21:05:00 +0700

    Adds Category model

---
+++ ASP.NET WebApp/ASPNetDocker/Models/Category.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace ASPNetDocker.Models
+{
+    public class Category
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public int Capacity { get; set; }
+    }
+}
